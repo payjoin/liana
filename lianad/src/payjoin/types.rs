c949090112413faafa12c90fa6db9babfccef31e
Armin Sabouri <armins88@gmail.com>
2025-07-09 12:41:17 -0400

    Remove unused receiver sender status enums

--- lianad/src/payjoin/types.rs
+++ lianad/src/payjoin/types.rs
@@ -1,47 +1,5 @@
 use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Clone, Copy, PartialEq)]
-pub enum PayjoinSenderStatus {
-    Pending = 0,
-    WaitingReceiver = 1,
-    Completed = 2,
-    // TODO: more specific enums for why it failed
-    Failed = 3,
-}
-
-#[derive(Debug, Clone, Copy, PartialEq, Serialize, Deserialize)]
-pub enum PayjoinReceiverStatus {
-    Pending = 0,
-    Signing = 1,
-    Completed = 2,
-    // TODO: more specific enums for why it failed
-    Failed = 3,
-}
-
-impl From<i32> for PayjoinSenderStatus {
-    fn from(status: i32) -> Self {
-        match status {
-            0 => PayjoinSenderStatus::Pending,
-            1 => PayjoinSenderStatus::WaitingReceiver,
-            2 => PayjoinSenderStatus::Completed,
-            3 => PayjoinSenderStatus::Failed,
-            _ => panic!("Invalid payjoin sender status: {}", status),
-        }
-    }
-}
-
-impl From<i32> for PayjoinReceiverStatus {
-    fn from(status: i32) -> Self {
-        match status {
-            0 => PayjoinReceiverStatus::Pending,
-            1 => PayjoinReceiverStatus::Signing,
-            2 => PayjoinReceiverStatus::Completed,
-            3 => PayjoinReceiverStatus::Failed,
-            _ => panic!("Invalid payjoin receiver status: {}", status),
-        }
-    }
-}
-
 #[derive(Debug, Clone, Copy, PartialEq, Serialize, Deserialize)]
 pub enum PayjoinStatus {
     Pending = 0,
